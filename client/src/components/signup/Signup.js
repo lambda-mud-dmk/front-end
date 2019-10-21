5fb67269239c81754063f48ac98af17952b523bc
kellswork <Bighead93+>
2019-10-21 20:19:08 +0100

    Implement user registration

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
-import Input from '../input/Input'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Input from '../input/Input';
+import PropTypes from 'prop-types';
+import Button from '../button/Button';
 
 export default class signup extends Component {
     state = {
         username: '',
         password: '',
-        email: ''
+        confirm_password: '',
+        password_error: '',
+        error: ''
     }
 
     handleChange = (event) => {
@@ -16,11 +20,44 @@ export default class signup extends Component {
         })
     }
 
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const { password, confirm_password, password_error, username} = this.state;
+        if (!username || username.length < 3) {
+            return this.setState({ error: 'username must be at least 3 characters long'})
+        }
+        if (!password || password.length < 6) {
+          return this.setState({
+            password_error: "password must be at least 5 characters long"
+          });
+        }
+        if (password !== confirm_password) {
+            return this.setState({
+                password_error: 'passwords do not match'
+            });
+        }
+
+        try {
+          const response = await axios.post(
+            "https://lambda-mud-test.herokuapp.com/api/registration",
+            {
+              username,
+              password
+            }
+          );
+          console.log(response);
+        } catch (error) {
+          console.error(error);
+        }
+
+        
+    }
+
     render() {
-        const { username, password, email } = this.state
+        const { username, password, confirm_password, error, password_error } = this.state
         return (
           <div>
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <h2>Create a New Account</h2>
               <div className="input-field">
                 <Input
@@ -29,19 +66,22 @@ export default class signup extends Component {
                   value={username}
                   handleChange={this.handleChange}
                 />
-                <Input
-                  type="email"
-                  name="email"
-                  value={email}
-                  handleChange={this.handleChange}
-                />
+                {error && <p> {error} </p>}
                 <Input
                   type="password"
                   name="password"
                   value={password}
                   handleChange={this.handleChange}
                 />
+                <Input
+                  type="password"
+                  name="confirm_password"
+                  value={confirm_password}
+                  handleChange={this.handleChange}
+                />
+                {password_error && <p> {password_error} </p>}
               </div>
+              <Button>signup</Button>
             </form>
           </div>
         );

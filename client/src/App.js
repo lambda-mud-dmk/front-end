f3fd2de921c398ca25ef688651aad7a34500b6c4
AwaMelvine <melvineawa@gmail.com>
2019-10-23 13:58:51 +0100

    Create game world component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Signup from './components/signup/Signup';
-import Login from './components/login/Login';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import GameWorld from "./components/game/GameWorld";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path='/registration'>
+        <Route path="/registration">
           <Signup />
         </Route>
-        <Route path='/login'>
+        <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/play">
+          <GameWorld />
         </Route>
       </Switch>
     </div>
---
+++ client/src/components/game/GameWorld.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function GameWorld() {
+  return (
+    <div>
+      <h2>Only logged in users</h2>
+    </div>
+  );
+}

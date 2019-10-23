bbdcc25a972cedb81a37c6dbc132b3ac3cd60b53
David Quiñones <daquinons@users.noreply.github.com>
2019-10-23 16:19:27 +0200

    Fix redirect

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <Switch>
-        <PrivateRoute path="/">
+        <PrivateRoute exact path="/">
           <GameWorld />
         </PrivateRoute>
         <Route path="/registration">

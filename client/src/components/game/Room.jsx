488b7cf080049b110b9aff6f709a63adba0c7cd4
AwaMelvine <melvineawa@gmail.com>
2019-10-24 09:25:42 +0100

    Display walls

--- client/src/components/game/Room.jsx
+++ client/src/components/game/Room.jsx
@@ -12,6 +12,18 @@ const RoomDiv = styled.div`
     props.direction === 'w'
       ? 'border-left: 1px solid #fcfcfc'
       : 'border-left: 1px solid transparent'};
+  ${props =>
+    props.direction === 'n'
+      ? 'border-top: 1px solid #fcfcfc'
+      : 'border-top: 1px solid transparent'};
+  ${props =>
+    props.direction === 'e'
+      ? 'border-right: 1px solid #fcfcfc'
+      : 'border-right: 1px solid transparent'};
+  ${props =>
+    props.direction === 's'
+      ? 'border-bottom: 1px solid #fcfcfc'
+      : 'border-bottom: 1px solid transparent'};
 `;
 
 const Player = styled.div`
@@ -21,7 +33,9 @@ const Player = styled.div`
   border-radius: 50%;
 `;
 
-const Room = ({ title, n_to, s_to, w_to, e_to }) => {
+const Room = props => {
+  console.log(props);
+  const { id, title, n_to, s_to, w_to, e_to } = props.room;
   let direction = '';
   if (n_to !== 0) {
     direction = 'n';

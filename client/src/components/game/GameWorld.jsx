3eae690113d18ca58ebbc411a6d2fa5fbeef2519
AwaMelvine <melvineawa@gmail.com>
2019-10-24 06:50:24 +0100

    Fetch worlds for frontend

--- client/src/components/game/GameWorld.jsx
+++ client/src/components/game/GameWorld.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import Controls from '../controls/Controls';
 import { GameDiv, GameMap, GameControls } from './GameWorldStyles';
@@ -6,15 +6,32 @@ import { GameDiv, GameMap, GameControls } from './GameWorldStyles';
 const apiUrl = 'http://127.0.0.1:8000/api/adv/rooms';
 // const apiUrl = "https://dmk-csbw1.herokuapp.com/api/adv/rooms";
 
-const GameWorld = () => {
-  return (
-    <GameDiv id="main">
-      <GameMap id="map">Map</GameMap>
-      <GameControls id="controls">
-        <Controls />
-      </GameControls>
-    </GameDiv>
-  );
-};
+class GameWorld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: []
+    };
+  }
+  componentDidMount() {
+    this.fetchRooms();
+  }
+
+  fetchRooms = async () => {
+    const response = await axios.get(`${apiUrl}/adv/rooms`);
+    console.log(response);
+  };
+
+  render() {
+    return (
+      <GameDiv>
+        <GameMap>Map</GameMap>
+        <GameControls id="controls">
+          <Controls />
+        </GameControls>
+      </GameDiv>
+    );
+  }
+}
 
 export default GameWorld;
